6a1d53c15574c9da265bdc71811015b1afdedce5
Harsh kumar <hk7206210@gmail.com>
2022-12-17 21:26:45 +0530

    add sites app

--- nginx.sh
+++ nginx.sh
@@ -2,10 +2,10 @@
 
 sudo apt install nginx -y
 
-sudo cp -rf app.conf /etc/nginx/sites-available/django_ci_cd
-# chmod 710 /var/lib/jenkins/workspace/django_ci_cd
+sudo cp -rf app.conf /etc/nginx/sites-available/app
+chmod 710 /var/lib/jenkins/workspace/django_ci_cd
 
-sudo ln -s /etc/nginx/sites-available/django_ci_cd /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
