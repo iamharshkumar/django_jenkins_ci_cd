995e603a30c0a79e8c409e4510aa928d15104bef
Harsh kumar <hk7206210@gmail.com>
2022-12-17 21:55:52 +0530

    remove access

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,7 @@
 sudo apt install nginx -y
 
 sudo cp -rf app.conf /etc/nginx/sites-available/app
-chmod 710 /var/lib/jenkins/workspace/django_ci_cd
+# chmod 710 /var/lib/jenkins/workspace/django_ci_cd
 
 sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
